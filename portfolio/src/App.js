244fd6535edf83550ec70b4bdee56bf58f0c711e
Dixita <dixitabutani29@gmail.com>
2025-02-04 09:22:35 +0000

    Update App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './components/Home';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
